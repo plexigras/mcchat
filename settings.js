043469efe652c88d131aa80654ce857e1e54ccdf
plexigras <plexigras@aol.com>
2016-07-31 16:50:51 +0200

    added version setting

--- settings.js
+++ settings.js
@@ -2,5 +2,6 @@ exports.mc = {
   host: "localhost",   // optional
   port: 25565,         // optional
   username: "email@example.com",
-  password: "12345678" // required for online servers
+  password: "12345678", // required for online servers
+  version: "1.10"
 };
