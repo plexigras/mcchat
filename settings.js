6527dc4728bfb92b8829d517c3c93981d123d747
plexigras <plexigras@aol.com>
2016-07-31 19:44:36 +0200

    fixed settings file

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 exports.mc = {
-  host: "localhost",   // optional
-  port: 25565,         // optional
-  username: "email@example.com",
-  password: "12345678", // required for online servers
-  version: "1.10"
+  host: "localhost", // optional
+  port: 25565,       // optional
+  username: "email@example.com", // mojang email login
+  password: "12345678",          // required for online servers
+  version: '1.10'    // version 1.7, 1.8, 1.9 and 1.10
 };
